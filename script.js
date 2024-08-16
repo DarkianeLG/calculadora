9d55373d308f785d3d1864cfbae021b2ea8a6d3e
Darkiane Lima <darkianelima13@gmail.com>
2024-08-16 09:11:27 -0300

    Criando o arquivo html

---
+++ script.js
@@ -0,0 +1,4 @@
+let resultado = document.querySelector(".resultado");
+let adicao = document.querySelector(".adicao");
+let adicao = document.querySelector(".adicao");
+let  = document.querySelector(".adicao");
